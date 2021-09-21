8b7a0f01b24f05615b325d5c386dc6cc0334390c
Jessica Lee <to.jessywlee@gmail.com>
2021-09-21 19:08:03 +0900

    Add: transaction component

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { Transaction } from './Transaction';
+import { GlobalContext }from '../context/GlobalState';
 
 export const TransactionList = () => {
+
+	const { transactions } = useContext(GlobalContext);
+
 	return (
 		<>
 			<h3>History</h3>
       <ul id="list" className="list">
-        <li className="minus">
-          Cash <span>-$400</span><button className="delete-btn">x</button>
-        </li>
+				{transactions.map(transaction => {
+					return(
+						<Transaction key={transaction.id} transaction={transaction}/>
+					)
+				})}
+        
       </ul>
 		</>
 	)
