b7e5257c57aa57fce43b287ae755a3df62a1a9ce
Jessica Lee <to.jessywlee@gmail.com>
2021-09-22 17:47:50 +0900

    Add: balance, income/expense, delete transaction

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,11 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (state, action) => {
 	switch(action.type) {
+		case 'DELETE_TRANSACTION':
+			return {
+				...state,
+				transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+			}
 		default: 
 		return state;
 	}
